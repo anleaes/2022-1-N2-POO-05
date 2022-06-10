1757a2fa5dc83e7f9c8f09027839edd7befadfb1
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-06-10 01:43:31 -0300

    mudanças na classe professor

--- professor.py
+++ professor.py
@@ -2,7 +2,7 @@ from funcionario import Funcionario
 
 class Professor(Funcionario):
         
-    def __init__(self,nome,cpf, endereco, telefone,graducao, salario, carga_horaria_semanal):
+    def __init__(self, nome, cpf, endereco, telefone, graducao, salario, carga_horaria_semanal):
         super().__init__(nome, cpf, endereco, telefone)
         self._graducao = graducao
         self._salario = salario
