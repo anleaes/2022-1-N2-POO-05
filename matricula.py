40ac920308560dd7d8b52773a3b18221840bed1f
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-06-10 01:43:02 -0300

    mudanças na classe matricula

--- matricula.py
+++ matricula.py
@@ -1,6 +1,6 @@
 class Matricula:
     
-    def __init__(self,codigo_matricula,tipo,data, aluno,disciplina):
+    def __init__(self, codigo_matricula, tipo, data, aluno, disciplina):
         self._codigo_matricula = codigo_matricula
         self._tipo = tipo
         self._data = data
