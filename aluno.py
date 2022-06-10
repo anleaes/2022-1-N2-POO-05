8013d76da3b8fab679ad35f32f671702d50564a6
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-06-10 01:38:51 -0300

    mudança aluno

--- aluno.py
+++ aluno.py
@@ -1,8 +1,10 @@
 class Aluno:
     
-    def __init__(self,nome,cpf,telefone,data_nascimento,endereco):
+    def __init__(self, nome, cpf, telefone, data_nascimento, endereco):
         self._nome = nome
         self._cpf = cpf
         self._telefone = telefone
         self._data_nascimento = data_nascimento
         self._endereco = endereco
+
+    
