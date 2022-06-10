2f1f8a2fab5bff766e2e4f261e5749886a5348c0
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-06-10 01:38:19 -0300

    mudança reitor

--- reitor.py
+++ reitor.py
@@ -2,6 +2,6 @@ from funcionario import Funcionario
 
 class Reitor(Funcionario):
         
-    def __init__(self,nome,cpf, endereco, telefone,salario):
+    def __init__(self, nome, cpf, endereco, telefone, salario):
         super().__init__(nome, cpf, endereco, telefone)
         self._salario = salario
