a4d572b0d748f5c40e740867df6668797f01adfd
rafael saraiva <saraiva_cmr@hotmail.com>
2022-06-11 16:42:51 -0300

    adicionei atributos a professor,coordenador e reitor

--- coordenador.py
+++ coordenador.py
@@ -2,6 +2,8 @@ from funcionario import Funcionario
 
 class Coordenador(Funcionario):
         
-    def __init__(self, nome, cpf, endereco, telefone, salario):
+    def __init__(self, nome, cpf, endereco, telefone, formacao, salario, numerodecursoscoordenados):
         super().__init__(nome, cpf, endereco, telefone)
+        self._formacao = formacao
         self._salario = salario
+        self._numerodecursoscoordenados = numerodecursoscoordenados
--- reitor.py
+++ reitor.py
@@ -2,6 +2,7 @@ from funcionario import Funcionario
 
 class Reitor(Funcionario):
         
-    def __init__(self, nome, cpf, endereco, telefone, salario):
+    def __init__(self, nome, cpf, endereco, telefone, salario, numerodecampusadministrados):
         super().__init__(nome, cpf, endereco, telefone)
         self._salario = salario
+        self._numerodecampusadministrados = numerodecampusadministrados
