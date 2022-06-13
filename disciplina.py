f972232aa9a35fa4e8ae5847202b32a21c7bb069
rafael saraiva <saraiva_cmr@hotmail.com>
2022-06-12 22:50:25 -0300

    ajustei umas coisas nas classes e adicionei outro metodo

--- disciplina.py
+++ disciplina.py
@@ -1,6 +1,6 @@
 class Disciplina:
     
-    def __init__(self, nome, semestre, codigo_disciplina, carga_horaria, turno, modalidade_disciplina, descricao, curso):
+    def __init__(self, nome, semestre, codigo_disciplina, carga_horaria, turno, modalidade_disciplina, descricao, curso, professor):
         self._nome = nome
         self._semestre = semestre
         self._codigo_disciplina = codigo_disciplina
@@ -9,3 +9,4 @@ class Disciplina:
         self._modalidade_disciplina = modalidade_disciplina
         self._descricao = descricao
         self._curso = curso
+        self._professor = professor
--- main.py
+++ main.py
@@ -12,20 +12,30 @@ from turma import Turma
 
 print("Sistema de Matricula de faculdade")
 
-
 aluno1 = Aluno("Fulano de Tal", 12345678910, "(51) 99999-8888", "12/06/2000", "Rua tal, número 1")
+
 coordenador1 = Coordenador("Vanderlei Luxemburgo",  90909090909, "rua avenida, numero 10", "(51) 90909-0909", "Doutorado", "10,000", 3)
+
 area1 = Area("Tecnologia", "Area dos cursos de Tecnologia da Informação", 1, coordenador1)
-disciplina1 = Disciplina("Programação Orientada a Objetos", "2022/1", 5, 94.6, "Noite", "Presencial", "Disciplina de POO", "TI")
+
 funcionario1 = Funcionario("Fulaninho de tal", 90909090908, "(51) 50505-5050", "funcionario1@uniritter.com")
-matricula1 = Matricula(1, "graduação", "12/02/2022", aluno1, disciplina1)
+
 professor1 = Professor("Professor fulano", 12544512346, "Rua tal, 123", "(51) 90909-9090", "Mestrado", 5000, 40, 10 )
+
 reitor1 = Reitor("Reitor Ciclano", 88884444222, "Rua do canto, 123", "(51) 70707-7070", 15000, 1)
-turma1 = Turma("TI-1", "Segunda-Feira", "19:10", "505A", professor1, aluno1, disciplina1)
+
 campus1 = Campus("Campus FAPA","Manoel Elias, 2000", 1, reitor1)
-curso1 = Curso("Analise de Sistemas", 12, "Superior tecnologico", "Presencial", "2,5 anos", professor1, area1)
+
+curso1 = Curso("Analise de Sistemas", 12, "Superior tecnologico", "Presencial", "2,5 anos", area1)
+
+disciplina1 = Disciplina("Programação Orientada a Objetos", "2022/1", 5, 94.6, "Noite", "Presencial", "Disciplina de POO", curso1, professor1)
+
+matricula1 = Matricula(1, "graduação", "12/02/2022", aluno1, disciplina1)
+
+turma1 = Turma("TI-1", "Segunda-Feira", "19:10", "505A", professor1, aluno1, disciplina1)
 
 def mostrar_aluno(self):
-    print("O aluno de nome ", {self._aluno_nome}, "tem o seguinte cpf ", {self._aluno_cpf}, "seu telefone é ", {self._aluno_telefone}, "nascido em ", {self._aluno_data_nascimento}, "e reside no endereço:", {self._aluno_endereco})
+    print("O aluno de nome ", {self._aluno._nome}, "tem o seguinte cpf ", {self._aluno._cpf}, "seu telefone é ", {self._aluno._telefone}, "nascido em ", {self._aluno._data_nascimento}, "e reside no endereço:", {self._aluno._endereco})
 
-
+def mostrar_curso(self):
+    print("O Curso de ", {self._curso._nome}, "de código ", {self._curso._código_curso}, "e tipo de formação é", {self._curso._formacao}, ".Esse curso é na modalidade", {self._curso._modalidade}, "com duração de", {self._curso._duracao},"faz parte da área",{self._curso._area})
